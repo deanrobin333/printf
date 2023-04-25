3116040ccac90a08087d70ed4935928a1784c363
deanrobin333 <deanrobin777@gmail.com>
2023-04-25 20:43:37 +0000

    task 3

--- handle_other_specifiers.c
+++ handle_other_specifiers.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
 
-int write_hex(unsigned long int num, const char *map_to, char flag_ch);
 /**
- * handle_u - handle format specifier "u"
- * @args: va_list containing the unsigned integer to print
+ * handle_u - helper function to print an unsigned integer
+ * @args: arguments
  * Return: number of characters printed
  */
 int handle_u(va_list args)
 {
-	unsigned long int num = va_arg(args, unsigned long int);
-	unsigned long int check = 1;
-	int length = 0;
+	unsigned int i, check, length, num;
+
+	i = va_arg(args, unsigned int);
+	check = 1;
+	length = 0;
+	num = i;
 
 	for (; num / check > 9; )
 		check *= 10;
@@ -31,23 +31,27 @@ int handle_u(va_list args)
 
 /**
  * handle_o - handle format specifier "o"
- * @args: va_list containing the unsigned integer to print
+ * @args: va_list containing the unsigned int to print in octal
  * Return: number of characters printed
  */
 int handle_o(va_list args)
 {
-	unsigned long int num = va_arg(args, unsigned long int);
-	unsigned long int check = 1;
-	int length = 0;
+	unsigned int i, length, num, oct[100];
 
-	for (; num / check > 7; )
-		check *= 8;
+	i = va_arg(args, unsigned int);
+	length = 0;
+	num = i;
 
-	for (; check != 0; )
+	while (num != 0)
 	{
-		length += _putchar('0' + num / check);
-		num %= check;
-		check /= 8;
+		oct[length] = num % 8;
+		num /= 8;
+		length++;
+	}
+
+	for (; length > 0; length--)
+	{
+		_putchar(oct[length - 1] + '0');
 	}
 
 	return (length);
@@ -55,59 +59,73 @@ int handle_o(va_list args)
 
 /**
  * handle_x - handle format specifier "x"
- * @args: va_list containing the unsigned integer to print
+ * @args: va_list containing the unsigned int to print in hexadecimal
+ * (lowercase letters)
  * Return: number of characters printed
  */
-
 int handle_x(va_list args)
 {
-	unsigned long int num = va_arg(args, unsigned long int);
+	unsigned int i, length, num, hex[100];
 
-	return (write_hex(num, HEX_LOWER, 'x'));
+	i = va_arg(args, unsigned int);
+	length = 0;
+	num = i;
+
+	while (num != 0)
+	{
+		hex[length] = num % 16;
+		num /= 16;
+		length++;
+	}
+
+	for (; length > 0; length--)
+	{
+		if (hex[length - 1] < 10)
+		{
+			_putchar(hex[length - 1] + '0');
+		}
+		else
+		{
+			_putchar(hex[length - 1] + 'W');
+		}
+	}
+
+	return (length);
 }
+
 /**
  * handle_X - handle format specifier "X"
- * @args: va_list containing the unsigned integer to print
+ * @args: va_list containing the unsigned int to print in hexadecimal
+ * (uppercase letters)
  * Return: number of characters printed
  */
-
 int handle_X(va_list args)
 {
-	unsigned long int num = va_arg(args, unsigned long int);
+	unsigned int i, length, num, hex[100];
 
-	return (write_hex(num, HEX_UPPER, 'X'));
-}
+	i = va_arg(args, unsigned int);
+	length = 0;
+	num = i;
 
-/**
- * write_hex - Converts an unsigned long int to a hexadecimal
- * @num: The unsigned long integer to convert to hexadecimal.
- * @map_to: A string containing the mapping of digits to characters.
- * @flag_ch: The flag character to prepend to the output, or '\0' if none
- * Return: The number of characters written to standard output.
- */
-int write_hex(unsigned long int num, const char *map_to, char flag_ch)
-{
-	int count = 0;
-
-	char buffer[sizeof(unsigned long int) * 2 + 1];
-	char *ptr = buffer + sizeof(buffer) - 1;
-	*ptr = '\0';
-
-	do {
-		*--ptr = map_to[num % 16];
-		num /= 16;
-		count++;
-	} while (num != 0);
-
-	if (flag_ch != '\0')
+	while (num != 0)
 	{
-		*--ptr = flag_ch;
-		*--ptr = '0';
-		count += 2;
+		hex[length] = num % 16;
+		num /= 16;
+		length++;
 	}
-	return write(STDOUT_FILENO, ptr, sizeof(buffer) - (ptr - buffer)) == -1
-		? -1
-		: count;
 
+	for (; length > 0; length--)
+	{
+		if (hex[length - 1] < 10)
+		{
+			_putchar(hex[length - 1] + '0');
+		}
+		else
+		{
+			_putchar(hex[length - 1] + '7');
+		}
+	}
 
+	return (length);
 }
+
